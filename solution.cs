ea3632d65e3ba0d32cb50f2b23cd59f85ce036e4
Kooreis <kooreis.official@gmail.com>
2024-05-06 02:27:55 -0500

    Created a PriorityQueue class with methods to add tasks to the queue (enqueue), remove tasks from the queue (dequeue), and check if the queue is empty (isEmpty). The enqueue method adds tasks in order of priority.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,33 @@
-class Task {
-    constructor(priority, name) {
-        this.priority = priority;
-        this.name = name;
+class PriorityQueue {
+    constructor() {
+        this.tasks = [];
+    }
+
+    enqueue(priority, name) {
+        let task = new Task(priority, name);
+        if (this.isEmpty()) {
+            this.tasks.push(task);
+        } else {
+            let added = false;
+            for (let i = 0; i < this.tasks.length; i++) {
+                if (task.priority < this.tasks[i].priority) {
+                    this.tasks.splice(i, 0, task);
+                    added = true;
+                    break;
+                }
+            }
+            if (!added) {
+                this.tasks.push(task);
+            }
+        }
+    }
+
+    dequeue() {
+        let value = this.tasks.shift();
+        return value;
+    }
+
+    isEmpty() {
+        return this.tasks.length == 0;
     }
 }
